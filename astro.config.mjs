1531506fef7e0e6eea3473d6b5664bb7c04e4bbd
Fernando <meirelesff@hotmail.com>
2025-06-06 14:04:07 -0300

    Ajustes nos dados e URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
 //  site: "http://inverno.iesp.uerj.br/",
   site: "https://fmeireles.com/",
-  base: "/escola",
+  base: "/escola/",
   integrations: [tailwind(), mdx(), sitemap()],
   i18n: {
     defaultLocale: "pt-br",
