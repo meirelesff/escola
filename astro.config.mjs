9b4a80273b0bea253432e9df1fa219dd79e7ca13
Fernando <meirelesff@hotmail.com>
2025-06-06 11:34:47 -0300

    Atualiza informações do site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://inverno.iesp.uerj.br/",
-  base: "/",
+//  site: "http://inverno.iesp.uerj.br/",
+  site: "https://fmeireles.com/",
+  base: "/escola",
   integrations: [tailwind(), mdx(), sitemap()],
   i18n: {
     defaultLocale: "pt-br",
